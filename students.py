4a756f1105f926ad7af9845e1214a25d59945559
Keian Barton <Keian.Barton@Civica.co.uk>
2018-01-11 15:35:05 +0000

    Basic program to add students

---
+++ students.py
@@ -0,0 +1,37 @@
+import pprint
+
+students = []
+
+
+def get_students_titlecase():
+    students_titlecase = []
+    for student in students:
+        students_titlecase = student["name"].title()
+    return students_titlecase
+
+
+def print_students_titlecase():
+    print(get_students_titlecase())
+
+
+def add_student(name, student_id=1):
+    student = {"name": name, "student_id": student_id}
+    students.append(student)
+
+
+def main():
+    while True:
+        user_input = input("Would you like to add a student? (Y or N)\n")
+        if user_input.upper() == 'N':
+            pprint.pprint(students)
+            break
+        elif user_input.upper() != 'Y':
+            break
+        else:
+            student_name = input("Enter student name: ")
+            student_id = input("Enter student ID: ")
+            add_student(student_name, int(student_id))
+            print_students_titlecase()
+
+
+main()
---
+++ ternary.py
@@ -0,0 +1,10 @@
+"""
+Exemplifying the ternary operator
+"""
+
+a = 2
+b = 1
+
+x = "foo" if a > b else "bar"
+
+print(x)
